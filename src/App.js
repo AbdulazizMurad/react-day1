9061633e5097f2caf3fe8fe937322e630deb0862
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-08-18 20:45:41 +0300

    updated icon react-day1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const productsDisplay = products.map((product) => {
     return (
       <div className="image1-container">
-        <img src={product.image} className="image1" />
+        <img src={product.image} alt={product.alt} className="image1" />
         <h2> {product.name}</h2>
         <p>{product.price}</p>
       </div>
--- src/products.js
+++ src/products.js
@@ -4,24 +4,28 @@ const products = [
     image:
       "https://sallysbakingaddiction.com/wp-content/uploads/2013/05/classic-chocolate-chip-cookies.jpg",
     price: "10 KD",
+    alt: "Chocolate Chip cookies",
   },
   {
     name: "Cute Cookies",
     image:
       "https://bitesbybianca.com/wp-content/uploads/2022/08/teddy-bear-cookies-7.jpg",
     price: "10 KD",
+    alt: "Cute cookies",
   },
   {
     name: "Vanilla Cookies",
     image:
       "https://cloudykitchen.com/wp-content/uploads/2022/12/stack-of-vanilla-cookies-800x1200.jpg",
     price: "12 KD",
+    alt: "Vanilla cookies",
   },
   {
     name: "Red-Velvet Cookies",
     image:
       "https://www.cookingclassy.com/wp-content/uploads/2012/08/red-velvet-cookies-33-600x900.jpg",
     price: "15 KD",
+    alt: "Red Velvet cookies",
   },
 ];
 export default products;
