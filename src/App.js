a7b37225499b9d0342f4d79746e1e9b0c8270cdd
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-08-18 20:30:46 +0300

    day1 of react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./assets/css/day1.css";
+import coffee from "./assets/media/caffee.jpg";
+import products from "./products";
 function App() {
+  const productsDisplay = products.map((product) => {
+    return (
+      <div className="image1-container">
+        <img src={product.image} className="image1" />
+        <h2> {product.name}</h2>
+        <p>{product.price}</p>
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Coffee and Beyond</h1>
+      <p> where cookie maniac gathers </p>
+      <img src={coffee} alt="coffee picture" className="imageDesign" />
+
+      <div className="images-container">{productsDisplay} </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    name: "Chocolate Chip Cookies",
+    image:
+      "https://sallysbakingaddiction.com/wp-content/uploads/2013/05/classic-chocolate-chip-cookies.jpg",
+    price: "10 KD",
+  },
+  {
+    name: "Cute Cookies",
+    image:
+      "https://bitesbybianca.com/wp-content/uploads/2022/08/teddy-bear-cookies-7.jpg",
+    price: "10 KD",
+  },
+  {
+    name: "Vanilla Cookies",
+    image:
+      "https://cloudykitchen.com/wp-content/uploads/2022/12/stack-of-vanilla-cookies-800x1200.jpg",
+    price: "12 KD",
+  },
+  {
+    name: "Red-Velvet Cookies",
+    image:
+      "https://www.cookingclassy.com/wp-content/uploads/2012/08/red-velvet-cookies-33-600x900.jpg",
+    price: "15 KD",
+  },
+];
+export default products;
